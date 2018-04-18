db01023ac0748da90c338c52bffc615e65310b4a
sriya90 <sriya.vishalakshy@gmail.com>
2018-04-18 15:05:23 -0500

    initial commit

---
+++ app/src/main/java/com/finalprojectandroid/sriyavishalakshy/eyesofhawk/MainActivity.java
@@ -0,0 +1,84 @@
+package com.finalprojectandroid.sriyavishalakshy.eyesofhawk;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+//import com.google.firebase.firestore.DocumentSnapshot;
+//import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mainToolbar;
+    private FirebaseAuth mAuth;
+   // private FirebaseFirestore firebaseFirestore;
+
+    private String current_user_id;
+    //private FloatingActionButton addPostBtn;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+       // firebaseFirestore = FirebaseFirestore.getInstance();
+
+        mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+        //setting the app title
+        getSupportActionBar().setTitle("Eyes Of Hawk");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser == null){
+            sendToLogin();
+
+        }
+
+    }
+    private void sendToLogin() {
+
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.action_logout_btn:
+                mAuth.signOut();
+                sendToLogin();
+         return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
